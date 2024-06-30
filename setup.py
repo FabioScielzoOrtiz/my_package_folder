7e1a207d120bdd5e5301e921350f496a188c0ef3
Your Name <you@example.com>
2024-06-30 20:21:55 +0200

    n

--- setup.py
+++ setup.py
@@ -5,13 +5,13 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="simplesumproduct",
-    version="0.0.1",
+    version="0.0.2",
     author="Fabio Scielzo Ortiz",
     author_email="fabioscielzo98@gmail.com",
     description="Stupid package for a tutorial on how to create a Python package.",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/FabioScielzoOrtiz/python_package_tutorial",   
+    url="https://github.com/FabioScielzoOrtiz/my_package_folder",   
     packages=find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
